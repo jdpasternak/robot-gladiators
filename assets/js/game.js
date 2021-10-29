8e447e9a6012be7235aa9c0ea2d01c55c47e968a
Jake Pasternak <jdp.pasternak@gmail.com>
2021-10-29 09:04:56 -1000

    Add prompt for name; add fight()

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named fight
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(`Your robot's name is ${playerName}.`);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
