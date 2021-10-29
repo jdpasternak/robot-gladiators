c9ba04ed443c0b8e2a14fc3f7bcd557150f91ea4
Jake Pasternak <jdp.pasternak@gmail.com>
2021-10-29 10:51:01 -1000

    Update fight() with conditional to check if either play is dead

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,44 @@
-// this creates a function named fight
-
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(`Your robot's name is ${playerName}.`);
+// You can also log multiple values at ones like this
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// fight();
+var fight = function () {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of `playerAttack` from the value of `enemyHealth and update the value in the `enemyHealth` variable.
+    enemyHealth -= playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
+    
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(`${enemyName} has died!`);
+    } else {
+        window.alert(`${enemyName} still has ${enemyHealth} health left.`);
+    }
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth and update the value in the `playerHealth` variable.
+    playerHealth -= enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`)
+    
+    // Check player's health
+    if (playerHealth <= 0) {
+        window.alert(`${playerName} has died!`);
+    } else {
+        window.alert(`${playerName} still has ${playerHealth} health left.`);
+    }
+    
+};
+
+fight();
